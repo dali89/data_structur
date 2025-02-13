80ed7ca45eb12b8f3da83907c40c14e79641d801
dali89 <dalivaishnav64@gmail.com>
2025-02-13 07:51:21 -0800

    Intersection code

---
+++ Array/Easy/Intersection of two array/Optimal/Intersection.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int n=nums1.size();
+        int n1=nums2.size();
+        vector<int> c;
+        while(i<n && j<n1){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else{
+                if (c.empty() || c.back() != nums1[i]) {
+                c.push_back(nums1[i]);}
+                i++;
+                j++;
+            }
+        }
+   return c;
+}
+};
+nt main() {
+    Solution solution;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    vector<int> result = solution.intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
